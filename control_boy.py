fe8e68ea19bb6b9cd66304a7c21a8c7bf6890d8a
DongGeunShin <sdg0896@gmail.com>
2023-10-19 10:43:18 +0900

    파일 가져오기

---
+++ control_boy.py
@@ -0,0 +1,66 @@
+from pico2d import *
+
+import game_world
+from grass import Grass
+from boy import Boy
+
+
+# Game object class here
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            boy.handle_event(event)
+
+
+def create_world():
+    global running
+    global grass
+    global team
+    global boy
+
+    running = True
+    # world = []
+
+    boy = Boy()
+    # world.append(boy)
+    game_world.add_object(boy, 1)
+
+    grass = Grass()
+    # world.append(grass)
+    game_world.add_object(grass, 0)
+
+
+def update_world():
+    # for o in world:
+    #     o.update()
+    # pass
+    game_world.update()
+
+
+def render_world():
+    clear_canvas()
+    # for o in world:
+    #     o.draw()
+    game_world.render()
+    update_canvas()
+
+
+open_canvas()
+create_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
+# finalization code
+close_canvas()
